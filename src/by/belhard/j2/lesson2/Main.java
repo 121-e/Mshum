828e42c8c56b702c6250a99e288ca4870313ab8a
MaxShumikhin <akitomc@gmail.com>
2019-04-05 21:31:38 +0300

    init

--- src/by/belhard/j2/lesson2/Main.java
+++ src/by/belhard/j2/lesson2/Main.java
@@ -3,6 +3,30 @@ package by.belhard.j2.lesson2;
 public class Main {
 
     public static void main(String[] args) {
-int x;
+  /*  int x=23;
+
+        /*if (x==20){
+            x--;
+        } else if (x==21) {
+            x++;
+        } else {
+            x = x / 2;
+        }
+       */
+
+        /*  System.out.println(x%2==0 ? x/2 : -x);*/
+
+        //      System.out.println(x);
+        for (int i = -2; ; i += 2) {   //for цикл
+
+if (i>8) break;
+            System.out.println(i);
+        }
+        System.out.println("end");
+
+       /* int[] ints = new int[]{1, 2, 5}; // for each цикл
+        for (int i : ints) {
+            System.out.println(i);
+        }*/
     }
 }
