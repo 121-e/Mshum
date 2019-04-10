ae19d410b63842b800470adbf2f4256974774f3e
MaxShumikhin <akitomc@gmail.com>
2019-04-10 20:06:21 +0300

    3

---
+++ src/by/belhard/j2/lesson3/Main.java
@@ -0,0 +1,15 @@
+package by.belhard.j2.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+       /* String array [] = new String [3];
+        array [0]="a";
+        array [2]="c";
+        System.out.println(array [1]);*/
+       /* или (сразу происходит инициализация)
+        String array [] = new String [] {"a", "b", "c"}; */
+
+       //двумерные массивы
+        String array [][] = new String [4][3] ; //4 строки 3 столбца
+    }
+}
