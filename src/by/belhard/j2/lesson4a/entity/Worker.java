58a883472e02c4c31582d82ee45fa25d34a3795a
MaxShumikhin <akitomc@gmail.com>
2019-04-12 21:26:53 +0300

    .

---
+++ src/by/belhard/j2/lesson4a/entity/Worker.java
@@ -0,0 +1,24 @@
+package by.belhard.j2.lesson4a.entity;
+// тема наследование
+public class Worker extends Person {
+
+    private int salary;
+
+
+    public Worker(String name, int age, Sex sex) {
+        super(name, age, sex);
+    }
+
+    @Override
+    public void toWork() {
+        System.out.println("Я работаю рабочим");
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
