fdb44366ab05a69392d0889a53ca65f449e0d1d5
121-e <121-e@mail.ru>
2019-04-26 21:18:31 +0300

    .

--- src/by/belhard/j2/lesson8/les3/Main.java
+++ src/by/belhard/j2/lesson8/les3/Main.java
@@ -14,6 +14,9 @@ public class Main {
             System.out.println(inputString.toUpperCase());
             System.out.println("Enter string");
 
+
+
+
         }
 
         System.out.println("Good buy");
